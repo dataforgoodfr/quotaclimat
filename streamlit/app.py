2062aac9937d177630d62ff33b26013f94f2dda5
Rambier Estelle <38014937+estellerambier@users.noreply.github.com>
2022-10-23 09:11:50 +0200

    init dashboard

---
+++ streamlit/app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from ..data_processing.read_format_deduplicate import (
+    deduplicate_extracts, read_and_format_all_data_dump)
+from data_analytics.data_coverage import fig_percentage_of_covered_time_by_keywords
+PATH_EXTRACTS_DATA_DUMPS = "data/keywords/"
+PATH_CHANNEL_METADATA = "data/channels.xlsx"
+
+
+def run():
+    df_all = read_and_format_all_data_dump(PATH_EXTRACTS_DATA_DUMPS, PATH_CHANNEL_METADATA)
+    # figures answering the questions
+    fig_percentage_of_covered_time_by_keywords_ = fig_percentage_of_covered_time_by_keywords(df_all)
+    # streamlit interface
+    st.title("Quota climat")
+    st.markdown("Welcome")
+    st.header("to fill")
+    st.plotly_chart(fig_percentage_of_covered_time_by_keywords_)
+
+if __name__ == "__main__":
+    run()
---
+++ utils/plotly_theme.py
@@ -0,0 +1,51 @@
+import plotly.express as px
+import plotly.graph_objects as go
+
+# defining theme
+# CONFIG AND THEMES
+COLOR_SEQUENCE = [
+    "rgb(230, 50, 24)",
+    "rgb(240, 73, 70)",
+    "rgb(243, 127, 125)",
+    "rgb(248, 182, 181)",
+    "rgb(209, 220, 197)",
+    "rgb(137, 168, 141)",
+    "rgb(59, 111, 66)",
+    "rgb(66, 66, 66)",
+]
+
+SMALL_SEQUENCE2 = [
+    "rgb(230, 50, 24)",
+    "rgb(59, 111, 66)",
+]
+
+WARMING_STRIPES_SEQUENCE = [
+    "#08306b",
+    "#08519c",
+    "#2171b5",
+    "#4292c6",
+    "#6baed6",
+    "#9ecae1",
+    "#c6dbef",
+    "#deebf7",
+    "#fee0d2",
+    "#fcbba1",
+    "#fc9272",
+    "#fb6a4a",
+    "#ef3b2c",
+    "#cb181d",
+    "#a50f15",
+    "#67000d",
+]
+
+COLOR_SEQUENCE = COLOR_SEQUENCE  # + px.colors.qualitative.Antique
+px.defaults.template = "plotly_white"
+px.defaults.color_discrete_sequence = COLOR_SEQUENCE
+
+THEME = go.layout.Template()
+THEME.layout.treemapcolorway = COLOR_SEQUENCE
+THEME.layout.sunburstcolorway = COLOR_SEQUENCE
+THEME.layout.colorway = COLOR_SEQUENCE
+THEME.layout.piecolorway = COLOR_SEQUENCE
+THEME.layout.font = {"family": "Helvetica"}
+px.defaults.template = THEME

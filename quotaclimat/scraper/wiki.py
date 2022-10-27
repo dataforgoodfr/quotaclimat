511a59cabbe931029812cab28d0b2ca86301f67b
gmguarino <gmguarino1@gmail.com>
2022-10-27 19:13:19 +0200

    wiki api data manager

---
+++ quotaclimat/scraper/wiki.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import wikipedia
+
+import os
+
+WIKI_FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+
+wikipedia.set_lang("fr")
+
+
+class WikiChannelDataManager:
+
+    def __init__(self, filepath):
+        self.channel_list = pd.read_excel(filepath)
+        print(self.channel_list.head(2))
+        self.channel_list = self.channel_list['CHANNEL_NAME']
+
+    
+    def search_channels(self):
+        for idx, channel in enumerate(self.channel_list[84:]):
+            try:
+                searches = wikipedia.search(channel)
+                if len(searches) == 0:
+                    print(f'Nothing found for {channel}')
+                    continue
+                page = wikipedia.page(searches[0], auto_suggest=False)
+                print(idx, page.url)
+            except wikipedia.exceptions.DisambiguationError:
+                try:
+                    searches = wikipedia.search(channel)
+                    page = wikipedia.page(searches[0] + ' (chaîne de télévision)', auto_suggest=False)
+                    print(idx, page.url)
+                except wikipedia.exceptions.PageError:
+                    print(f'Nothing found for {channel}')
+            
+
+
+if __name__ == '__main__':
+    manager = WikiChannelDataManager(os.path.join(WIKI_FILE_PATH, '../../data/channels.xlsx'))
+    manager.search_channels()

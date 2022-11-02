52bd86a5ce7223ec62e2f5d0cb00d8157aeb03c5
Rambier Estelle <38014937+estellerambier@users.noreply.github.com>
2022-11-02 11:15:07 +0100

    fix: PEP8 linting

--- pages/1_Résultats_principaux.py
+++ pages/1_Résultats_principaux.py
@@ -1,17 +1,17 @@
 import streamlit as st
 
-
-from quotaclimat.data_analytics.data_coverage import \
-    (fig_percentage_of_covered_time_by_keywords, fig_time_coverage_of_extracts)
+from quotaclimat.data_analytics.data_coverage import (
+    fig_percentage_of_covered_time_by_keywords, fig_time_coverage_of_extracts)
 from quotaclimat.data_processing.read_format_deduplicate import (
     deduplicate_extracts, read_and_format_all_data_dump)
 
 PATH_EXTRACTS_DATA_DUMPS = "data/keywords/"
 PATH_CHANNEL_METADATA = "data/channels.xlsx"
 
+
 @st.cache(allow_output_mutation=True)
 def figures_computation():
-    
+
     df_all = read_and_format_all_data_dump(
         PATH_EXTRACTS_DATA_DUMPS, PATH_CHANNEL_METADATA
     )
@@ -20,11 +20,10 @@ def figures_computation():
         fig_percentage_of_covered_time_by_keywords(df_all, PATH_CHANNEL_METADATA)
     )
 
-    fig_time_coverage_of_extracts_ = (
-        fig_time_coverage_of_extracts(df_all)
-    )
+    fig_time_coverage_of_extracts_ = fig_time_coverage_of_extracts(df_all)
     fig_percentage_of_covered_time_by_keywords_, fig_time_coverage_of_extracts_
     return fig_percentage_of_covered_time_by_keywords_, fig_time_coverage_of_extracts_
+
 
 st.header("Couverture mediatique")
 st.sidebar.markdown("# Resultat principaux")

50fd44e82c04cbbdbcd818395c4ea0959bb462b7
Rambier Estelle <38014937+estellerambier@users.noreply.github.com>
2022-11-03 08:26:58 +0100

    fix: add check for deduplication

--- quotaclimat/data_processing/read_format_deduplicate.py
+++ quotaclimat/data_processing/read_format_deduplicate.py
@@ -14,7 +14,7 @@ def columns_names_to_camel_case(df):
 
 
 def read_and_format_all_data_dump(
-    path_folder="../data/keywords/", path_channel_metadata="../data/channels.xlsx"
+    path_folder="../data/keywords/", path_channel_metadata=None
 ):
     df_all = pd.DataFrame()
     list_all_files = [f for f in listdir(path_folder) if isfile(join(path_folder, f))]
@@ -61,13 +61,17 @@ def read_and_format_one(path_file=None, path_channels=None, data=None, name=None
         .drop(columns=["ORIGIN", "START CHUNK", "END CHUNK", "DATE"])
     )
     data = columns_names_to_camel_case(data)
+    data = deduplicate_extracts(data)
     return data
 
 
-def deduplicate_extracts(df):
+def deduplicate_extracts(df: pd.DataFrame):
     deduplicate_ids = [
         x for x in df.columns.tolist() if x not in ["keyword", "text", "highlight"]
     ]
+    # the deduplication assumed all sample are floored at pair minutes. The following line check if it holds
+    assert ~(df.date.dt.minute % 2).sum()
+    # this groupby all ids but keyword and text, collecting the keywords present in duplicated extract
     df_dedup = df.groupby(deduplicate_ids).agg(
         {"keyword": [set, pd.Series.nunique], "text": "first"}
     )

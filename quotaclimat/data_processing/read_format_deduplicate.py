fc02a7570a27016ef226ada6889c5bacda245dfc
Rambier Estelle <38014937+estellerambier@users.noreply.github.com>
2022-11-03 08:26:58 +0100

    fix: add check for deduplication

--- quotaclimat/data_processing/read_format_deduplicate.py
+++ quotaclimat/data_processing/read_format_deduplicate.py
@@ -3,8 +3,10 @@ from os import listdir
 from os.path import isfile, join
 
 import pandas as pd
+
 # TODO
-# reconsiliate france3 with local fr3 code name 
+# reconsiliate france3 with local fr3 code name
+
 
 def columns_names_to_camel_case(df):
     df.columns = [x.replace(" ", "_").lower() for x in df.columns]
@@ -12,8 +14,7 @@ def columns_names_to_camel_case(df):
 
 
 def read_and_format_all_data_dump(
-    path_folder="../data/keywords/", 
-    path_channel_metadata=None
+    path_folder="../data/keywords/", path_channel_metadata=None
 ):
     df_all = pd.DataFrame()
     list_all_files = [f for f in listdir(path_folder) if isfile(join(path_folder, f))]
@@ -23,20 +24,21 @@ def read_and_format_all_data_dump(
     return df_all.reset_index()
 
 
-def read_and_format_one(path_file = None, path_channels = None,data = None,name = None):
+def read_and_format_one(path_file=None, path_channels=None, data=None, name=None):
 
     if data is None:
         data = pd.read_excel(path_file)
 
     if path_channels is not None:
         channels = pd.read_excel(path_channels)
-        data = data.merge(channels,on="CHANNEL")
+        data = data.merge(channels, on="CHANNEL")
     else:
-        data = data.rename(columns = {"CHANNEL":"CHANNEL_NAME"})
+        data = data.rename(columns={"CHANNEL": "CHANNEL_NAME"})
 
     data = (
-        data
-        .assign(date=lambda x: pd.to_datetime(x["DATE"], format="%Y-%m-%dT%H-%M-%S"))
+        data.assign(
+            date=lambda x: pd.to_datetime(x["DATE"], format="%Y-%m-%dT%H-%M-%S")
+        )
         .assign(time=lambda x: x["date"].dt.time)
         .assign(
             time_of_the_day=lambda x: x["time"].map(
@@ -51,19 +53,25 @@ def read_and_format_one(path_file = None, path_channels = None,data = None,name 
         .assign(duration=lambda x: 2)
         .assign(
             keyword=lambda x: x["path_file"].map(
-                lambda y: y.rsplit("_", 1)[-1].replace(".xlsx", "") if name is None else name
+                lambda y: y.rsplit("_", 1)[-1].replace(".xlsx", "")
+                if name is None
+                else name
             )
         )
         .drop(columns=["ORIGIN", "START CHUNK", "END CHUNK", "DATE"])
     )
     data = columns_names_to_camel_case(data)
+    data = deduplicate_extracts(data)
     return data
 
 
-def deduplicate_extracts(df):
+def deduplicate_extracts(df: pd.DataFrame):
     deduplicate_ids = [
         x for x in df.columns.tolist() if x not in ["keyword", "text", "highlight"]
     ]
+    # the deduplication assumed all sample are floored at pair minutes. The following line check if it holds
+    assert ~(df.date.dt.minute % 2).sum()
+    # this groupby all ids but keyword and text, collecting the keywords present in duplicated extract
     df_dedup = df.groupby(deduplicate_ids).agg(
         {"keyword": [set, pd.Series.nunique], "text": "first"}
     )

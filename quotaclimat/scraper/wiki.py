3e52185278cdcedb8543049b24dafb556198a76b
gmguarino <gmguarino1@gmail.com>
2022-11-04 20:41:28 +0100

    output dataframe

--- quotaclimat/scraper/wiki.py
+++ quotaclimat/scraper/wiki.py
@@ -26,13 +26,19 @@ class WikiChannelDataManager:
 
     def get_html_summary_table(self, html):
         soup = BeautifulSoup(html, 'lxml')
-        rows = soup.body.find('table').findAll("tr", attrs={'class':''})
+        try:
+            rows = soup.body.find('table').findAll("tr", attrs={'class':''})
+        except AttributeError:
+            raise TypeError
         return rows
 
     def get_group(self, rows): 
         for row in rows:
             if 'Propriétaire' in row.text:
-                return row.find('div').text.strip()
+                try:
+                    return row.find('div').text.strip()
+                except AttributeError:
+                    return None
 
     def remove_parenthesis_substr(self, text):
         return re.sub("[\(\[].*?[\)\]]", "", text)
@@ -40,7 +46,17 @@ class WikiChannelDataManager:
     def get_channel_type(self, rows):
         for row in rows:
             if 'Statut' in row.text:
-                return self.remove_parenthesis_substr(row.find('div').text.strip()).split(' ')
+                try:
+                    return self.remove_parenthesis_substr(row.find('div').text.strip()).split(' ')
+                except AttributeError:
+                    return None
+
+    def parse_type_list(self, type_list):
+        theme = [el for el in type_list if el.title() in ('Généraliste', 'Thématique')]
+        ownership = [el for el in type_list if el.lower() in ('publique', 'privée')]
+        theme = None if len(theme)==0 else theme[0]
+        ownership = None if len(ownership)==0 else ownership[0]
+        return theme, ownership
 
     def get_audiences_table(self, html):
         soup = BeautifulSoup(html, 'lxml')
@@ -52,12 +68,17 @@ class WikiChannelDataManager:
                     print(contains_caption.text)
                     return table
         #mw-content-text > div.mw-parser-output > table:nth-child(98) > tbody
+
+    def post_process_results(self, results):
+        results.type = results.type.map({'Généraliste': 'general', 'Thématique': 'news'})
+        results.privatization_status = results.privatization_status.map({'publique': 'public', 'privée': 'private'})
+        results.index.name = 'channel'
+        return results
     
     def search_channels(self):
         results = {}
-        for idx, channel in enumerate(self.channel_list[0:10]):
+        for idx, channel in enumerate(self.channel_list[:10]):
             channel_name = channel.replace(' ', '_').lower()
-            results[channel_name] = {}
             try:
                 searches = wikipedia.search(channel)
                 if len(searches) == 0:
@@ -72,13 +93,18 @@ class WikiChannelDataManager:
                     print(idx, page.url)
                 except wikipedia.exceptions.PageError:
                     print(f'Nothing found for {channel}')
-            html = self.get_html(page)
-
-            rows = self.get_html_summary_table(html)
-            results[channel_name]['group'] = self.get_group(rows)
-            results[channel_name]['type'] = self.get_channel_type(rows)
-
-        return results
+            try:
+                results[channel_name] = {}
+                html = self.get_html(page)
+                rows = self.get_html_summary_table(html)
+                type_list = self.get_channel_type(rows)
+                theme, ownership = self.parse_type_list(type_list)
+                results[channel_name]['type'] = theme
+                results[channel_name]['privatization_status'] = ownership
+                results[channel_name]['group'] = self.get_group(rows)
+            except ValueError:
+                continue
+        return self.post_process_results(pd.DataFrame.from_dict(results, orient='index'))
 
 
 if __name__ == '__main__':
@@ -86,5 +112,4 @@ if __name__ == '__main__':
     results = manager.search_channels()
     if not os.path.isdir(os.path.join(WIKI_FILE_PATH, '../../data/channels/')):
         os.mkdir(os.path.join(WIKI_FILE_PATH, '../../data/channels/'))
-    with open(os.path.join(WIKI_FILE_PATH, '../../data/channels/results.json'), 'w', encoding='utf8') as jf:
-        json.dump(results, jf, indent=2, ensure_ascii=False)
+    results.to_csv(os.path.join(WIKI_FILE_PATH, '../../data/channels/results.csv'))

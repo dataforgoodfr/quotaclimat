165f27c271a2d91f025d701d42a34744aa00c7db
__plb__ <plbayart@gmail.com>
2022-11-17 00:21:22 +0100

    refactoring dataframe creator module

--- quotaclimat/data_processing/create_df_television_news_analyser.py
+++ quotaclimat/data_processing/create_df_television_news_analyser.py
@@ -5,42 +5,43 @@ Created on Wed Nov 16 18:57:23 2022
 @author: Elise
 """
 
-import pandas as pd
 import os
-import json
+import pandas as pd
+from tqdm.notebook import tqdm
 
-path_qclimat = "C:/Users/elise/Documents/Data for good/Quota climat/"
-path_data_tele = path_qclimat + "data/television-news-analyser-main/data-news-json/"
+def create_csv(path_qclimat="C:/Users/elise/Documents/Data for good/Quota climat/"):
+    path_data_tele = os.path.join(path_qclimat, "data", "television-news-analyser", "data-news-json")
+    
+    data_medias = pd.DataFrame(columns=[
+           'title', 'description', 'date', 'order', 'presenter',
+           'authors', 'editor', 'editorDeputy', 'url', 'urlTvNews',
+           'containsWordGlobalWarming','media','year','month','day',
+    ])
 
+    #lire les fichiers de chaque dossier
+    list_medias = os.listdir(path_data_tele)
+    for media in tqdm(list_medias):
+        list_year = os.listdir(os.path.join(path_data_tele, media))
+        for year in tqdm(list_year):
+            list_month = os.listdir(os.path.join(path_data_tele, media, year))
+            for month in list_month:
+                list_day = os.listdir(os.path.join(path_data_tele, media, year, month))
+                for day in list_day:
 
-data_medias = pd.DataFrame(columns=['title', 'description', 'date', 'order', 'presenter', 'authors',
-       'editor', 'editorDeputy', 'url', 'urlTvNews',
-       'containsWordGlobalWarming','media','year','month','day'])
+                    #ouvrir le json et en faire un dataframe
+                    file = os.listdir(os.path.join(path_data_tele, media, year, month, day))
+                    df = pd.read_json(os.path.join(path_data_tele, media, year, month, day, file[0]), lines=True)
 
-#lire les fichiers de chaque dossier
-list_medias = os.listdir(path_data_tele)
-for media in list_medias:
-    list_year = os.listdir(path_data_tele + media + '/')
-    for year in list_year:
-        list_month = os.listdir(path_data_tele + media + '/' + year + '/')
-        for month in list_month:
-            list_day = os.listdir(path_data_tele + media + '/' + year + '/' + month + '/')
-            for day in list_day:
-                
-                #ouvrir le json et en faire un dataframe
-                file = os.listdir(path_data_tele + media + '/' + year + '/' + month + '/' + day + '/')
-                f = open(path_data_tele + media + '/' + year + '/' + month + '/' + day + '/' + file[0], encoding='utf-8')
-                json_file = [json.loads(line) for line in f]
-                df = pd.DataFrame.from_dict(json_file, orient='columns')
-                
-                #ajouter les infos du média et de la date
-                df['media'] = media[6:] #enlever "media="
-                df['year'] = year[5:]
-                df['month'] = month[6:]
-                df['day'] = day[4:]
-                
-                #ajouter les données de ce fichier json au dataframe global
-                data_medias = pd.concat([data_medias,df])
-                
-                
-data_medias.to_csv(path_qclimat+'data/df_3medias.csv',index=False)
+                    #ajouter les infos du média et de la date
+                    df['media'] = media[6:] #enlever "media="
+                    df['year'] = year[5:]
+                    df['month'] = month[6:]
+                    df['day'] = day[4:]
+                    
+                    #pour résoudre un futurewarning concernant une colonne avec uniquement des booléens
+                    df["containsWordGlobalWarming"] = df["containsWordGlobalWarming"].astype("boolean")
+                    
+                    #ajouter les données de ce fichier json au dataframe global
+                    data_medias = pd.concat([data_medias,df])
+
+    data_medias.to_csv(os.path.join(path_qclimat, 'data', 'df_3medias.csv'),index=False)

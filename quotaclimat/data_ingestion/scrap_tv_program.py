75d012ef3983c39df0615d4bb1df10b05db265a3
Rambier Estelle <38014937+estellerambier@users.noreply.github.com>
2022-11-25 21:28:59 +0100

    feat: started working on job

--- quotaclimat/data_ingestion/scrap_tv_program.py
+++ quotaclimat/data_ingestion/scrap_tv_program.py
@@ -4,10 +4,7 @@ from datetime import date
 import pandas as pd
 import requests
 import xmltodict
-
-from categorization_program_type import \
-    MAPPING_PROGRAMMES_CATEGORIES
-
+from categorization_program_type import MAPPING_PROGRAMMES_CATEGORIES
 
 
 def extract_tv_program(

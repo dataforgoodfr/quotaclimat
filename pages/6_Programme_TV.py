e025549e5b2876ccac02b5dceb1c3f772ac7824d
Rambier Estelle <38014937+estellerambier@users.noreply.github.com>
2022-11-26 11:09:17 +0100

    fix: hardcode tv_program data path & linting

--- pages/6_Programme_TV.py
+++ pages/6_Programme_TV.py
@@ -1,13 +1,16 @@
 # %%
 # from pathlib import Path
-import pandas as pd
-import streamlit as st
-import plotly.express as px
-from datetime import datetime
 import os
-from quotaclimat.data_processing.extract_and_process_tv_program import get_tv_programs_next_days
+from datetime import datetime
 
-PATH_TV_PROGRAMS = "data/20221108_20221112_Programme_TV.csv"
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+from quotaclimat.data_processing.extract_and_process_tv_program import \
+    get_tv_programs_next_days
+# TODO: make dynamic!
+PATH_TV_PROGRAMS = "data_public/20221125_20221202_Programme_TV.csv"
 
 
 @st.cache(allow_output_mutation=True)
@@ -28,7 +31,8 @@ if bool_print:
 channels = st.multiselect(
     "De quelle chaine de télévision voulez vous visualiser le programme TV ?",
     df.channel_name.unique(),
-    default="TF1")
+    default="TF1",
+)
 
 
 col1, col2 = st.columns(2)
@@ -36,29 +40,30 @@ col1, col2 = st.columns(2)
 with col1:
     date_beginning = st.date_input(
         "Quelle est la date initiale ?",
-        value=datetime.strptime(df.start.min().split(" ")[0], '%Y-%m-%d'),
-        min_value=datetime.strptime(df.start.min().split(" ")[0], '%Y-%m-%d'),
-        max_value=datetime.strptime(df.start.max().split(" ")[0], '%Y-%m-%d')
+        value=datetime.strptime(df.start.min().split(" ")[0], "%Y-%m-%d"),
+        min_value=datetime.strptime(df.start.min().split(" ")[0], "%Y-%m-%d"),
+        max_value=datetime.strptime(df.start.max().split(" ")[0], "%Y-%m-%d"),
     )
 with col2:
     date_end = st.date_input(
         "Quelle est la date finale ?",
-        value=datetime.strptime(df.stop.max().split(" ")[0], '%Y-%m-%d'),
+        value=datetime.strptime(df.stop.max().split(" ")[0], "%Y-%m-%d"),
         min_value=date_beginning,
-        max_value=datetime.strptime(df.stop.max().split(" ")[0], '%Y-%m-%d')
+        max_value=datetime.strptime(df.stop.max().split(" ")[0], "%Y-%m-%d"),
     )
 
 fig = px.timeline(
-        df[
-            (df.channel_name.isin(channels))
-            & (df.start >= date_beginning.strftime("%Y-%m-%d")) & (df.stop <= date_end.strftime("%Y-%m-%d"))
-            ],
-        x_start='start',
-        x_end="stop",
-        y="channel_name",
-        color="category_text",
-        hover_data=["title", "subtitle", "start", "stop", "category_text"]
-    )
+    df[
+        (df.channel_name.isin(channels))
+        & (df.start >= date_beginning.strftime("%Y-%m-%d"))
+        & (df.stop <= date_end.strftime("%Y-%m-%d"))
+    ],
+    x_start="start",
+    x_end="stop",
+    y="channel_name",
+    color="category_text",
+    hover_data=["title", "subtitle", "start", "stop", "category_text"],
+)
 st.plotly_chart(fig)
 
 

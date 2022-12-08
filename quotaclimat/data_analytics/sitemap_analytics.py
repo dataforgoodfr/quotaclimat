0483cb417bbb5a9b2da1d39b3b902ecc8e320591
Beef <bast.gauthier@gmail.com>
2022-12-08 22:30:15 +0100

    Ajout du graphe détaillé par keywords, avec pourcentages

--- quotaclimat/data_analytics/sitemap_analytics.py
+++ quotaclimat/data_analytics/sitemap_analytics.py
@@ -46,6 +46,33 @@ def plot_media_count_comparison(df, keywords: list, keywords_comp: list):
     )
     return fig
 
+def plot_media_count_comparison_all_kw(df, keywords: list):
+
+    dict_data = {}
+    for kw in keywords:
+        dict_data[kw] = df[df.news_title.str.contains(kw)]
+        dict_data[kw]["keyword"] = kw
+
+    df_keywords_1 = pd.concat([value for value in dict_data.values()])
+    # Count the data for the plot 
+    df_to_plot = df_keywords_1.groupby(["media","keyword"]).count().reset_index()
+    # Count the proportion of a keyword in a media
+    total_media = df_keywords_1.groupby(["media"]).count().news_title
+    for media in total_media.items():
+        mask = df_to_plot.media.values == media[0]
+        df_to_plot.loc[mask,"keyword_percent_in_media"] = df_to_plot.loc[mask,"news_title"]/media[1]*100
+    df_to_plot.keyword_percent_in_media = df_to_plot.keyword_percent_in_media.apply(lambda x : f'{x:.2f}')
+
+    fig = px.bar(
+        df_to_plot,
+        x="media",
+        y="news_title",
+        color="keyword",
+        hover_data =["keyword_percent_in_media"],
+        title="Comparaison du nombre de titre d'articles comprenant un mot d'une des deux listes (avec doublons)",
+    )
+
+    return fig
 
 def plot_comparison_of_temporal_total_count(df, keywords: list, keywords_comp: list):
 

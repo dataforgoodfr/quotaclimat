9186e8f78fc625ae6f8c5b50a17a186eacb1f4d9
Rambier Estelle <38014937+estellerambier@users.noreply.github.com>
2023-01-15 10:20:11 +0100

    feat: add letelegramme

--- quotaclimat/data_ingestion/config_sitmap.py
+++ quotaclimat/data_ingestion/config_sitmap.py
@@ -44,19 +44,62 @@ SITEMAP_CONFIG = {
         "sitemap_url": "https://www.lexpress.fr/sitemap_actu_1.xml",
         "regex_section": r"^https:\/\/www\.lexpress\.fr\/(?P<section>[\/\w-]*)\/.+$",
     },
+    "letelegramme": {
+        "sitemap_index": None,
+        "sitemap_url": "https://www.letelegramme.fr/metasitemap_news.xml",
+        "regex_section": r"^https:\/\/www\.letelegramme\.fr\/(?P<section>[\/\w-]*)\/.+$",
+    },
 }
 
 MEDIA_CONFIG = {
-    "bfmtv": {"site_url": "https://www.bfmtv.com/", "type": "tv"},
-    "lefigaro": {"site_url": "https://www.lefigaro.fr/", "type": "webpress"},
-    "francetvinfo": {"site_url": "https://www.francetvinfo.fr/", "type": "tv"},
-    "lemonde": {"site_url": "https://www.lemonde.fr/", "type": "webpress"},
-    "20_minutes": {"site_url": "https://www.20minutes.fr/", "type": "webpress"},
+    "bfmtv": {
+        "site_url": "https://www.bfmtv.com/",
+        "type": "tv",
+        "coverage": "national",
+    },
+    "lefigaro": {
+        "site_url": "https://www.lefigaro.fr/",
+        "type": "webpress",
+        "coverage": "national",
+    },
+    "francetvinfo": {
+        "site_url": "https://www.francetvinfo.fr/",
+        "type": "tv",
+        "coverage": "national",
+    },
+    "lemonde": {
+        "site_url": "https://www.lemonde.fr/",
+        "type": "webpress",
+        "coverage": "national",
+    },
+    "20_minutes": {
+        "site_url": "https://www.20minutes.fr/",
+        "type": "webpress",
+        "coverage": "national",
+    },
     "liberation": {
         "site_url": "https://www.liberation.fr/arc/outboundfeeds/",
         "type": "webpress",
+        "coverage": "national",
     },
-    "nouvel_obs": {"site_url": "https://www.nouvelobs.com/", "type": "webpress"},
-    "le_point": {"site_url": "https://www.lepoint.fr/", "type": "webpress"},
-    "lexpress": {"site_url": "https://www.lexpress.fr/", "type": "webpress"},
+    "nouvel_obs": {
+        "site_url": "https://www.nouvelobs.com/",
+        "type": "webpress",
+        "coverage": "national",
+    },
+    "le_point": {
+        "site_url": "https://www.lepoint.fr/",
+        "type": "webpress",
+        "coverage": "national",
+    },
+    "lexpress": {
+        "site_url": "https://www.lexpress.fr/",
+        "type": "webpress",
+        "coverage": "national",
+    },
+    "letelegramme": {
+        "site_url": "https://www.letelegramme.fr/",
+        "type": "webpress",
+        "coverage": "regional",
+    },
 }

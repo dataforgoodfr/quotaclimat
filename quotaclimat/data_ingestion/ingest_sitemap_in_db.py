f6f27aa643d87c71464674a484674d3568293709
Rambier Estelle <38014937+estellerambier@users.noreply.github.com>
2023-04-01 21:28:20 +0200

    fix: transform before insertion

--- quotaclimat/data_ingestion/ingest_sitemap_in_db.py
+++ quotaclimat/data_ingestion/ingest_sitemap_in_db.py
@@ -1,7 +1,8 @@
 import logging
 from argparse import ArgumentParser
 
-from postgres.insert_existing_data_example import insert_data_in_sitemap_table
+from postgres.insert_existing_data_example import (
+    insert_data_in_sitemap_table, transformation_from_dumps_to_table_entry)
 from quotaclimat.data_ingestion.config_sitmap import (MEDIA_CONFIG,
                                                       SITEMAP_CONFIG)
 from quotaclimat.data_ingestion.scrap_sitemap import \
@@ -17,10 +18,11 @@ def run():
     for media, sitemap_conf in SITEMAP_CONFIG.items():
         if media != "bfmtv":  # TO REMOVE for dev: quick fail
             continue
-        #try:
+        # try:
         df = query_one_sitemap_and_transform(media, sitemap_conf)
-        insert_data_in_sitemap_table(df, DB_PWD)
-        #except Exception as err:
+        df_to_insert = transformation_from_dumps_to_table_entry(df)
+        insert_data_in_sitemap_table(df_to_insert, DB_PWD)
+        # except Exception as err:
         #    logging.error("Could not ingest data in db for %s: %s" % (media, err))
         #    continue
 

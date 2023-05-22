7d2490ad7edbde5e5c0e2a172a43c32a8c93ebe6
Rambier Estelle <38014937+estellerambier@users.noreply.github.com>
2023-05-22 22:03:10 +0200

    feat: backup to scale way locally tested

---
+++ quotaclimat/data_ingestion/scaleway_db_backups.py
@@ -0,0 +1,68 @@
+import logging
+from argparse import ArgumentParser
+from datetime import datetime, timedelta
+
+import psycopg2
+
+from quotaclimat.data_processing.sitemap.queries import (
+    query_all_articles_titles_between_two_dates, query_data_coverage)
+
+parser = ArgumentParser()
+parser.add_argument("-p", "--dbpwd")
+parser.add_argument("-k", "--keyid")
+parser.add_argument("-a", "--accesskey")
+
+args = parser.parse_args()
+DB_PWD = args.dbpwd
+KEY_ID = args.keyid
+ACCESS_KEY = args.accesskey
+
+CONN = psycopg2.connect(
+    database="quotaclimat",
+    user="root",
+    password=DB_PWD,
+    host="212.47.253.253",
+    port="49157",
+)
+
+
+def write_backup_between_dates(date_min: datetime, date_max: datetime):
+    """Fetch the data from the db and write a flat file to S3
+
+    Args:
+        date_min (datetime): date of the first element to backup
+        date_max (datetime): date of the last element to backup
+    """
+    df_to_backup = query_all_articles_titles_between_two_dates(CONN, date_min, date_max)
+    dict_ecs = {
+        "anon": False,
+        "key": KEY_ID,
+        "s3_additional_kwargs": {"ACL": "private"},
+        "secret": ACCESS_KEY,
+        "use_ssl": False,
+        "client_kwargs": {
+            "endpoint_url": "https://s3.fr-par.scw.cloud",
+            "region_name": "fr-par",
+        },
+    }
+    path_back_up = "s3://quotaclimat-s3/backups/v1/from_%s_to_%s.parquet" % (
+        date_min.strftime("%Y%m%d"),
+        date_max.strftime("%Y%m%d"),
+    )
+    df_to_backup.to_parquet(path_back_up, storage_options=dict_ecs)
+    logging.info("DB Data was backup to %s" % path_back_up)
+
+
+def write_init_backup():
+    date_min, date_max = query_data_coverage(CONN)
+    write_backup_between_dates(date_min, date_max)
+
+
+def write_past_week_backup():
+    today = datetime.today()
+    a_week_ago = today - timedelta(days=7)
+    write_backup_between_dates(today, a_week_ago)
+
+
+if __name__ == "__main__":
+    write_past_week_backup()

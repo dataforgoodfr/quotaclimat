eb1b0a012eb7856bceb3a5a714bf402e78809eda
Paul Leclercq <paleclercq@gmail.com>
2024-02-11 22:59:22 +0100

    fix: test

--- test/sitemap/test_mediatree_utils.py
+++ test/sitemap/test_mediatree_utils.py
@@ -18,23 +18,21 @@ def test_get_yesterday():
 
 def test_get_date_range():
     range = get_date_range(1681214197, 1681646197)
-    expected = pd.DatetimeIndex(['2023-04-11 11:56:37', '2023-04-11 19:56:37',
-         '2023-04-12 03:56:37', '2023-04-12 11:56:37',
-         '2023-04-12 19:56:37', '2023-04-13 03:56:37',
-         '2023-04-13 11:56:37', '2023-04-13 19:56:37',
-         '2023-04-14 03:56:37', '2023-04-14 11:56:37',
-         '2023-04-14 19:56:37', '2023-04-15 03:56:37',
-         '2023-04-15 11:56:37', '2023-04-15 19:56:37',
-         '2023-04-16 03:56:37', '2023-04-16 11:56:37'],
-        dtype='datetime64[ns]', freq='8H')
+    expected = pd.DatetimeIndex(['2023-04-11 11:56:37', '2023-04-11 23:56:37',
+         '2023-04-12 11:56:37', '2023-04-12 23:56:37',
+         '2023-04-13 11:56:37', '2023-04-13 23:56:37',
+         '2023-04-14 11:56:37', '2023-04-14 23:56:37',
+         '2023-04-15 11:56:37', '2023-04-15 23:56:37',
+         '2023-04-16 11:56:37'],
+        dtype='datetime64[ns]', freq='12H')
     assert len(expected) == len(range) # ValueError: the 'dtype' parameter is not supported in the pandas implementation of any()
 
 def test_get_default_date_range():
     # test default
     range = get_date_range(get_yesterday(), None)
-    assert len(range) == 5
+    assert len(range) == 4
 
     # test with function
     (start_date_to_query, end_epoch) = get_start_end_date_env_variable_with_default()
     range = get_date_range(start_date_to_query, end_epoch)
-    assert len(range) == 5
+    assert len(range) == 4

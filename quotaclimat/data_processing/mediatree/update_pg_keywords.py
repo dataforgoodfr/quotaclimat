7922cc72caf3b0a9f74be0780a20995ed0e6b305
RDiPiazza <raphael.dipiazza@pm.me>
2024-11-24 12:11:50 +0100

    fixed issue

--- quotaclimat/data_processing/mediatree/update_pg_keywords.py
+++ quotaclimat/data_processing/mediatree/update_pg_keywords.py
@@ -94,10 +94,10 @@ def update_keywords(session: Session, batch_size: int = 50000, start_date : str 
                 ,number_of_biodiversite_causes_directes
                 ,number_of_biodiversite_consequences
                 ,number_of_biodiversite_solutions_directes
-                ,channel_title=channel_title
-                ,number_of_keywords_climat=new_number_of_keywords_climat
-                ,number_of_keywords_biodiversite=new_number_of_keywords_biodiversite
-                ,number_of_keywords_ressources=new_number_of_keywords_ressources
+                ,channel_title
+                ,new_number_of_keywords_climat
+                ,new_number_of_keywords_biodiversite
+                ,new_number_of_keywords_ressources
                 ,number_of_changement_climatique_constat_no_hrfp
                 ,number_of_changement_climatique_causes_directes_no_hrfp
                 ,number_of_changement_climatique_consequences_no_hrfp

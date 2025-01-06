21d1285853b57ee2cb47696ac601c130fff953b2
Paul Leclercq <paleclercq@gmail.com>
2025-01-06 12:16:03 +0100

    fix: date import - cron monitor (#304)
    * fix: date import - cron monitor
    
    * fix: api-to-s3 - default security days to 7 with env variable
    
    * fix test

--- test/sitemap/test_mediatree_utils.py
+++ test/sitemap/test_mediatree_utils.py
@@ -15,23 +15,6 @@ def test_get_yesterday():
     logging.info(f"yesterday_string {yesterday_string}")
     assert '00:00:00' in yesterday_string
 
-
-def test_get_date_range():
-    range = get_date_range(1681214197, 1681646197) # 11 to 16 april --> 6 days
-
-    assert 6  == len(range) # ValueError: the 'dtype' parameter is not supported in the pandas implementation of any()
-
-def test_get_default_date_range():
-    # test default
-    range = get_date_range(get_yesterday(), None)
-    assert len(range) == 1
-
-    # test with function
-    (start_date_to_query, end_epoch) = get_start_end_date_env_variable_with_default()
-    range = get_date_range(start_date_to_query, end_epoch)
-    assert len(range) == 1
-
-
 def test_is_it_tuesday():
     date = pd.Timestamp("2024-02-13 15:34:28")
     assert is_it_tuesday(date) == True
@@ -43,3 +26,29 @@ def test_get_end_of_month():
     assert get_end_of_month("2024-04-01") == "2024-04-30"
     assert get_end_of_month("2024-02-01") == "2024-02-29"
     assert get_end_of_month("2024-02-15") == "2024-02-29"
+
+
+
+def test_get_start_end_date_env_variable_with_default():
+    start_date = None
+    
+    assert get_start_end_date_env_variable_with_default(start_date, minus_days=1) == (get_yesterday(), None)
+
+def test_get_start_end_date_env_variable_with_start_date_value():
+    start_date = 1734508085
+    number_of_previous_days = 7
+    start_date_minus_days = start_date - (number_of_previous_days * 24 * 60 * 60)
+
+    assert get_start_end_date_env_variable_with_default(start_date, minus_days=number_of_previous_days) == (int(start_date), start_date_minus_days)
+
+def test_get_start_end_date_with_get_date_range():
+    start_date = 1734508085
+    number_of_previous_days = 7
+    (start,end) = get_start_end_date_env_variable_with_default(start_date, minus_days=number_of_previous_days)
+
+    expected = pd.DatetimeIndex(['2024-12-11', '2024-12-12', '2024-12-13', '2024-12-14', '2024-12-15', '2024-12-16', '2024-12-17', '2024-12-18'],
+                        dtype='datetime64[ns]', freq='D')
+    
+    output = get_date_range(start,end)
+    assert len(output) == number_of_previous_days + 1
+    pd.testing.assert_index_equal(output, expected)

78454900e84c19ff13e05cec638a004be9bad625
Paul Leclercq <paleclercq@gmail.com>
2025-01-06 12:02:22 +0100

    fix test

--- test/sitemap/test_mediatree_utils.py
+++ test/sitemap/test_mediatree_utils.py
@@ -15,23 +15,6 @@ def test_get_yesterday():
     logging.info(f"yesterday_string {yesterday_string}")
     assert '00:00:00' in yesterday_string
 
-
-def test_get_date_range():
-    range = get_date_range(1681214197, 1681646197) # 11 to 16 april --> 6 days
-
-    assert 6  == len(range) # ValueError: the 'dtype' parameter is not supported in the pandas implementation of any()
-
-def test_get_default_date_range():
-    # test default
-    range = get_date_range(get_yesterday(), None)
-    assert len(range) == 1
-
-    # test with function
-    (start_date_to_query, end_epoch) = get_start_end_date_env_variable_with_default()
-    range = get_date_range(start_date_to_query, end_epoch)
-    assert len(range) == 1
-
-
 def test_is_it_tuesday():
     date = pd.Timestamp("2024-02-13 15:34:28")
     assert is_it_tuesday(date) == True

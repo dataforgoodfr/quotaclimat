5bfd7b5f52a0bec6821dc0d6da0156b3b07c1883
Paul Leclercq <paleclercq@gmail.com>
2025-02-12 13:05:35 +0100

    wip: ci test

---
+++ my_dbt_project/pytest_tests /test_dbt_model_homepage.py
@@ -0,0 +1,39 @@
+import pytest
+import psycopg2
+import os
+import subprocess
+
+@pytest.fixture(scope="module")
+def db_connection():
+    conn = psycopg2.connect(
+        dbname=os.getenv("POSTGRES_DB",""),
+        user= os.getenv("POSTGRES_USER",""),
+        password=os.getenv("POSTGRES_PASSWORD",""),
+        host=os.getenv("POSTGRES_HOST",""),
+        port=os.getenv("POSTGRES_PORT","")
+    )
+    yield conn
+    conn.close()
+
+def run_dbt_command(command_args):
+    """Helper function to run dbt commands."""
+    result = subprocess.run(
+        ["poetry", "run", "dbt",  *command_args, "--project-dir", "/app/my_dbt_project"],
+        capture_output=True, text=True
+    )
+    print(result.stdout)  # Print dbt logs for debugging
+    assert result.returncode == 0, f"dbt command failed: {result.stderr}"
+
+def test_homepage_environment_by_media_by_month(db_connection):
+    """Test the materialized view using dbt and pytest."""
+
+    run_dbt_command(["seed"])
+
+    run_dbt_command(["run", "--models", "homepage_environment_by_media_by_month"])
+
+    cur = db_connection.cursor()
+    cur.execute("SELECT COUNT(*) FROM public.homepage_environment_by_media_by_month;")
+    count = cur.fetchone()[0]
+    cur.close()
+
+    assert count > 0, "No data found in the materialized view!"

0c976681a12ce15cf642b21134acf25e44a5931a
Paul Leclercq <paleclercq@gmail.com>
2025-05-26 15:12:51 +0200

    Fix/dbt: category should come from dictionary table based on keyword's theme (#389)
    * wip
    
    * fix: remove unnecessary group by element introduced in #362
    
    * wip
    
    * fix: use dictionary to get category
    
    * fix: default to keywords channel_title if program metadata has no entries for keyword ts

--- my_dbt_project/models/dashboards/core_query_thematics_keywords.sql
+++ my_dbt_project/models/dashboards/core_query_thematics_keywords.sql
@@ -1,25 +1,15 @@
 {{ config(
     materialized='incremental'
-    ,unique_key=['start','channel_title']
+    ,unique_key=['week','channel_title']
   )
 }}
 
     SELECT
-      pm.channel_title,
+      COALESCE(pm.channel_title, k.channel_title) AS channel_title,
       DATE_TRUNC('week', k.start) :: date AS week,
       -- Dictionary metadata
       d.high_risk_of_false_positive,
-      d.solution,
-      d.consequence,
-      d.cause,
-      d.general_concepts,
-      d.statement,
-      d.crisis_climate,
-      d.crisis_biodiversity,
-      d.crisis_resource,
-      d.categories,
-      d.themes,
-      d.language,
+      COALESCE(NULLIF(d.category, ''), 'Transversal') AS category,
       CASE
         WHEN LOWER(kw ->> 'theme') LIKE '%solution%' THEN TRUE
         ELSE FALSE
@@ -40,7 +30,6 @@
           WHEN LOWER(kw ->> 'theme') LIKE '%constat%' THEN TRUE
           ELSE FALSE
       END AS is_statement,
-	    category, --from dictionary table
       -- Crise type selon le thème
       CASE
         WHEN LOWER(kw ->> 'theme') LIKE '%climat%' THEN 'Crise climatique'
@@ -49,7 +38,6 @@
         ELSE 'Autre'
       END AS crise_type,
       kw ->> 'theme' AS theme,
-     -- COALESCE(NULLIF(TRIM(kw ->> 'category'), ''), 'Transversal') AS category, --legacy category would can be not up to date.
       kw ->> 'keyword' AS keyword,
       COUNT(*) AS count
     FROM
@@ -86,36 +74,19 @@ LEFT JOIN public.program_metadata pm ON k.channel_program = pm.channel_program
       AND CAST(pm.program_grid_end AS date) -- Expand keywords
 ,
       json_array_elements(k.keywords_with_timestamp :: json) AS kw -- Join dictionary on keyword
-      LEFT JOIN public.dictionary d ON d."keyword" = kw ->> 'keyword' -- Exclude indirect themes
-
-   -- Unnest categories array into one line per category
-    LEFT JOIN LATERAL UNNEST(
-        COALESCE(
-            d.categories,
-            ARRAY['Transversal']
-        )
-    ) AS category ON TRUE
+      LEFT JOIN public.dictionary d ON d."keyword" = kw ->> 'keyword'AND d."theme" = kw ->> 'theme'
 
 WHERE
       LOWER(kw ->> 'theme') NOT LIKE '%indirect%'
       AND k."country" = 'france'
+      
    
 GROUP BY
-      pm.channel_title,
+      COALESCE(pm.channel_title, k.channel_title),
       DATE_TRUNC('week', k.start) :: date,
       -- Dictionary metadata
       d.high_risk_of_false_positive,
-      d.solution,
-      d.consequence,
-      d.cause,
-      d.general_concepts,
-      d.statement,
-      d.crisis_climate,
-      d.crisis_biodiversity,
-      d.crisis_resource,
-      d.categories,
-      d.themes,
-      d.language,
+      COALESCE(NULLIF(d.category, ''), 'Transversal'),
       CASE
           WHEN LOWER(kw ->> 'theme') LIKE '%solution%' THEN TRUE
           ELSE FALSE
@@ -153,6 +124,6 @@ GROUP BY
 	    category
    
 ORDER BY
-      pm.channel_title,
+      channel_title,
       week,
       crise_type

0907aeee8e8f8799bb10bde4b2da6e3559b07cf2
gmguarino <gmguarino1@gmail.com>
2025-05-28 12:12:59 +0200

    add test arte

--- my_dbt_project/pytest_tests/test_dbt_model_homepage.py
+++ my_dbt_project/pytest_tests/test_dbt_model_homepage.py
@@ -135,6 +135,32 @@ def test_core_query_thematics_keywords_values(db_connection):
         row_trimmed = row[:-1]
         assert row_trimmed == expected_trimmed, f"Unexpected values: {row}"
 
+    with db_connection.cursor() as cur:
+        cur.execute("""
+            SELECT channel_title, week, sum_duration_minutes, crise_type, theme, category, keyword, count,
+            high_risk_of_false_positive
+            FROM public.core_query_thematics_keywords
+            WHERE channel_title = 'Arte' AND keyword = 'responsable' AND theme = 'biodiversite_solutions'
+            ORDER BY channel_title DESC
+            LIMIT 1
+        """)
+        row = cur.fetchone()
+
+        expected= (
+        'Arte',
+        datetime.date(2025, 1, 27),
+        455,
+        'Crise de la biodiversité',
+        'biodiversite_solutions',
+        'Transversal',
+        'responsable',
+        1,
+        None)
+        
+        expected_trimmed = expected[:-1] 
+        row_trimmed = row[:-1]
+        assert row_trimmed == expected_trimmed, f"Unexpected values: {row}"
+
 def test_core_query_thematics_keywords_values_i8n(db_connection):
 
     with db_connection.cursor() as cur:

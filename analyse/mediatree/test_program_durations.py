f57279cdea7abeee053aec7f7dcc053f40727500
gmguarino <gmguarino1@gmail.com>
2026-02-04 10:43:29 +0100

    fdsg

--- analyse/mediatree/test_program_durations.py
+++ analyse/mediatree/test_program_durations.py
@@ -430,6 +430,7 @@ if __name__ == "__main__":
                 records.extend(get_coverages_for_day(day, country, channel_programs))
 
     df = pd.DataFrame.from_records(records)
+    print(df)
     df_group = df.groupby(["date", "country", "channel_name"]).agg(
         {"coverage": "mean", "total_results": "sum", "total_minutes": "sum"}
     )

87c067b9f6b016df00cda9a36d2c5b710b982a0b
gmguarino <gmguarino1@gmail.com>
2026-02-23 12:11:41 +0100

    add lemonde web entrypoint

---
+++ docker-entrypoint-lemonde-web.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+cd /app
+echo "============================================"
+echo "Running Factiva migrations with alembic"
+echo "PWD: $(pwd)"
+echo "Listing alembic_factiva files:"
+ls -la alembic_factiva || true
+ls -la alembic_factiva/versions || true
+echo "Using config file: alembic_factiva.ini"
+
+# Check Python and alembic are available
+python --version
+alembic --version || true
+
+# Run Alembic migrations
+echo "Run migrations to ensure database is up to date"
+alembic -c alembic_factiva.ini upgrade head
+
+echo "============================================"
+echo "Starting S3 Factiva to PostgreSQL processor"
+python quotaclimat/data_ingestion/lemonde_ftp/ftp_to_s3.py
+python quotaclimat/data_processing/lemonde_ftp/s3_to_postgre/s3_lemonde_to_postgre.py

891e361067b62d328985186b9743af3fb5c4ee5a
gmguarino <gmguarino1@gmail.com>
2026-02-27 15:59:56 +0000

    separate belgium  by language

--- quotaclimat/data_processing/mediatree/i8n/belgium/channel_titles.py
+++ quotaclimat/data_processing/mediatree/i8n/belgium/channel_titles.py
@@ -1,14 +1,6 @@
 channel_titles_belgium = {
     "la-premiere": "La Premi\u00e8re",
-    "canvas": "Canvas",
-    "radio-2": "RADIO 2",
-    "vtm": "VTM",
-    "radio-1": "RADIO 1",
-    "stu-bru": "STU BRU",
-    "qmusic": "QMUSIC",
-    "play": "Play",
     "bel-rtl": "BEL RTL",
     "vivacite": "Vivacit\u00e9",
     "ln-radio": "LN RADIO",
-    "vrt1": "VRT 1",
 }

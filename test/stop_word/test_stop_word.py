e0b6eeca110001fadb7ea7a9aa28a443b1e507aa
gmguarino <gmguarino1@gmail.com>
2026-03-25 17:48:32 +0100

    fix test

--- test/stop_word/test_stop_word.py
+++ test/stop_word/test_stop_word.py
@@ -78,11 +78,11 @@ def test_stop_word_get_top_keywords_by_channel():
                 "channel_title": "France 2",
                 "count": 20,
             },{
-                "keyword": "climatique",
-                "theme": "changement_climatique_constat",
-                "channel_title": "France 2",
-                "count": 20,
-            },{
+            #     "keyword": "climatique",
+            #     "theme": "changement_climatique_constat",
+            #     "channel_title": "France 2",
+            #     "count": 20,
+            # },{
                 "keyword": "sortie des énergies fossiles",
                 "theme": "attenuation_climatique_solutions",
                 "channel_title": "France 2",

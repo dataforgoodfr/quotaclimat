21c62cd746df41293857d896821fe6c757e85eb6
gmguarino <gmguarino1@gmail.com>
2026-04-14 17:24:40 +0200

    parse env variable from path

--- insfrastructure/bin/tofu_wrapper.sh
+++ insfrastructure/bin/tofu_wrapper.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 tofu_cmd() {
-    source .env && source live/$1/$2/.env.secrets && tofu -chdir=live/$1/$2 $3
+    source .env && source live/$1/$2/.env.secrets && export TF_VAR_environment=$2 && tofu -chdir=live/$1/$2 $3
 }

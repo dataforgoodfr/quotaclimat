c735a1d8cf4c74deea5becb4db4b147582acf776
Giuseppe Guarino <40060221+gmguarino@users.noreply.github.com>
2026-05-12 16:58:36 +0200

    Rrs tests (#571)
    * add rrs optional dependencies
    
    * add first rrs tests
    
    * follow usual process steps
    
    * improve clustering
    
    * add timeseries method
    
    * add rrs dependencies
    
    * remove absorption
    
    * use both splits
    
    * switch to mistral
    
    * mistral dependency

---
+++ quotaclimat/data_processing/rrs/insecurity/dictionary.py
@@ -0,0 +1,30 @@
+keywords = [
+    "zones de non-droit", #=> "Zones" et/oui "non-droit" (exemple d'ajustement)
+    "ensauvagement",
+    "explosion de violence",
+    "justice laxiste",
+    "police empêchée",
+    "à feu et à sang",
+    "on ne peut plus sortir",
+    "les médias n’en parlent pas",
+    "Insécurité explose",
+    "Ne sont plus en sécurité",
+    "Envoyer l'armée",
+    "Justice laxiste",
+    "Protéger le peuple",
+    "A cause de la politique migratoire",
+    "Commis par des étrangers",
+    "submersion",
+    "invasion",
+    "territoires perdus",
+    "reconquête",
+]
+
+correlate_with = [
+    "encore",
+    "toujours",
+    "chaque jour",
+    "preuve que",
+    "ça montre que",
+    "encore une fois",
+]

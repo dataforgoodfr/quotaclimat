d8ee45628a326de44bfff79589d04ac9acd6ee38
Giuseppe Guarino <40060221+gmguarino@users.noreply.github.com>
2026-06-10 15:22:14 +0200

    Ome/improve api to s3 (#586)
    * add async to api to s3
    
    * remove brazil from prod
    
    * add bel-fla to config

--- quotaclimat/data_ingestion/labelstudio/configs.py
+++ quotaclimat/data_ingestion/labelstudio/configs.py
@@ -3,5 +3,6 @@ db_config = [
     {"database": "labelstudio-climate-poland-prod-db", "countries": {1: "poland"}},
     {"database": "labelstudio-climate-spain-prod-db", "countries": {1: "spain"}},
     {"database": "labelstudio-climate-belgium-prod-db", "countries": {2: "belgium"}},
+    {"database": "labelstudio-climate-belgium-flanders-prod-db", "countries": {1: "belgium"}},
     {"database": "labelstudio-climate-germany-prod-db", "countries": {1: "germany"}},
 ]

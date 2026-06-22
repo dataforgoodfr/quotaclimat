1688e45696f00a3d6f0f7d77cadd56795fad7c5a
gmguarino <gmguarino1@gmail.com>
2026-06-22 15:21:21 +0200

    add outputs

---
+++ infrastructure/live/advertising/template/outputs.tf
@@ -0,0 +1,17 @@
+output "project_api_key_access_key" {
+  value = scaleway_iam_api_key.project_api_key.access_key
+}
+
+output "project_api_key_secret_key" {
+  value     = scaleway_iam_api_key.project_api_key.secret_key
+  sensitive = true
+}
+
+output "mediatree_videos_api_key_access_key" {
+  value = scaleway_iam_api_key.mediatree_videos_api_key.access_key
+}
+
+output "mediatree_videos_api_key_secret_key" {
+  value     = scaleway_iam_api_key.mediatree_videos_api_key.secret_key
+  sensitive = true
+}

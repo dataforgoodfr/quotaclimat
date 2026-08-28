03c63d172f32feb87fb8f6c0c0595b371488cf08
AdamaDiawara <diawaraad97@gmail.com>
2026-08-28 17:07:32 +0200

    Nettoyage

--- infrastructure/live/rrs/template/registry.tf
+++
@@ -1,6 +0,0 @@
-resource "scaleway_registry_namespace" "rrs" {
-  name       = "rrs-${var.environment}"
-  project_id = scaleway_account_project.project.id
-  region     = "fr-par"
-  is_public  = false
-}
--- rrs/scripts/migrate_and_seed.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-alembic -c rrs/alembic.ini upgrade head
-python -m rrs.dictionary.upsert_subjects
-python -m rrs.dictionary.upsert_dictionary

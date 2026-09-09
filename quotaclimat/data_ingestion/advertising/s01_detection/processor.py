be4418d93a508a56e4da690c00f404670910d158
apibrac <alexis.pibrac@gmail.com>
2026-09-09 15:12:53 +0000

    Reorganize steps

--- quotaclimat/data_ingestion/advertising/s01_detection/processor.py
+++ quotaclimat/data_ingestion/advertising/s01_detection/processor.py
@@ -1,18 +1,14 @@
 import json
 import logging
 from concurrent.futures import ProcessPoolExecutor
-from datetime import datetime, timedelta
+from datetime import datetime
 from functools import partial
-from zoneinfo import ZoneInfo
 
 from ..tools.fingerprint_tools.compare import FingerprintsCompare
 from ..tools.fingerprints import fingerprinter
 from ..tools.interactive_tqdm import interactive_tqdm
-from ..tools.mediatree.bucket_mediatree import (
-    download_days_audio_parts,
-    get_s3_filesystem,
-)
 from ..tools.segments import Segment
+from .e00_download_audio import download_all_audio_parts
 from .e02_create_chunks import ChunkCreator, ChunkCreatorJob
 from .e03_already_identified_advertising import run_chunk_identification
 from .e04_group_chunks import group_chunks
@@ -82,64 +78,13 @@ async def processor(
     #### Download all weeks audio segments
 
     with timings.measure("audio_download"):
-        audio_files = await download_days_audio_parts(
-            fs=get_s3_filesystem(),
+        chunks_creator_jobs = await download_all_audio_parts(
             channel=channel,
-            # list all days between start_date and end_date
-            days=[
-                start_date.date() + timedelta(days=i)
-                for i in range((end_date.date() - start_date.date()).days)
-            ],
-            dest_dir="./.cache/mediatree",
-            max_concurrent_downloads=10,
+            start_date=start_date,
+            end_date=end_date,
         )
         logger.info(
-            f"Downloaded {len(audio_files)} audio files for channel {channel} between {start_date} and {end_date}"
-        )
-
-        # audio file names look like: franceinfotv_2026-09-07T04-36-00Z_2026-09-07T04-38-00Z.mp3
-        filename_dt_format = "%Y-%m-%dT%H-%M-%SZ"
-        audio_segments = [
-            (
-                Segment(
-                    start_date=datetime.strptime(
-                        f.split("/")[-1].split(".")[0].split("_")[1],
-                        filename_dt_format,
-                    ).replace(tzinfo=ZoneInfo("UTC")),
-                    end_date=datetime.strptime(
-                        f.split("/")[-1].split(".")[0].split("_")[2],
-                        filename_dt_format,
-                    ).replace(tzinfo=ZoneInfo("UTC")),
-                    channel=channel,
-                ),
-                f,
-            )
-            for f in audio_files
-        ]
-
-    #### Analyze the week partition
-
-    chunks_creator_jobs: list[ChunkCreatorJob] = []
-
-    for i, (segment, audio_file_path) in enumerate(audio_segments):
-        is_previous_contiguous = (
-            i > 0 and audio_segments[i - 1][0].end_date == segment.start_date
-        )
-        next_contiguous = (
-            audio_segments[i + 1]
-            if (
-                i < len(audio_segments) - 1
-                and audio_segments[i + 1][0].start_date == segment.end_date
-            )
-            else None
-        )
-        chunks_creator_jobs.append(
-            ChunkCreatorJob(
-                segment=segment,
-                audio_file_path=audio_file_path,
-                has_previous_segment=is_previous_contiguous,
-                next_audio_file_path=next_contiguous[1] if next_contiguous else None,
-            )
+            f"Downloaded {len(chunks_creator_jobs)} audio files for channel {channel} between {start_date} and {end_date}"
         )
 
     #### Audio processing
@@ -166,10 +111,12 @@ async def processor(
 
             chunks: list[Chunk] = [
                 chunk
-                for segment, _ in audio_segments
+                for job in chunks_creator_jobs
                 for chunk in (
                     Chunk.from_dict(d)
-                    for d in json.loads(chunk_cache.get(segment.identifier + ".json"))
+                    for d in json.loads(
+                        chunk_cache.get(job.segment.identifier + ".json")
+                    )
                 )
             ]
 
@@ -201,7 +148,7 @@ async def processor(
     #### Database storage
 
     with timings.measure("clean_pre_existing_occurrences"):
-        clean_pre_existing_detections([segment for segment, _ in audio_segments])
+        clean_pre_existing_detections([job.segment for job in chunks_creator_jobs])
 
     with timings.measure("database_storage"):
         database_storage_save(fragments, fingerprint_hash=fingerprint_hash)

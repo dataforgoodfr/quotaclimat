eca8ce3420c184d11086b6a5163a82e9dee5eb1c
Giuseppe Guarino <40060221+gmguarino@users.noreply.github.com>
2026-09-15 10:11:17 +0200

    Add rrs-insecurity Kestra flow (#642)
    * add ivana ssh key
    
    * add prod flow
    
    * always execute flow
    
    * change subject name

--- rrs/dictionary/subjects.py
+++ rrs/dictionary/subjects.py
@@ -3,5 +3,5 @@ from rrs.dictionary.subject import insecurity
 
 subjects = {
     "climate": {"keywords": THEME_KEYWORDS, "title": "Changement Climatique"},
-    "insecurity": {"keywords": insecurity.keywords, "title": "Immigration"}
+    "insecurity": {"keywords": insecurity.keywords, "title": "Migration"}
 }

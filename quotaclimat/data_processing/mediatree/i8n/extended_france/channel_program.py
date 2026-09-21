88a2d559dbf33d0de28d9d447b13c01b17a08394
gmguarino <gmguarino1@gmail.com>
2026-09-21 11:42:41 +0200

    fix as list of records

--- quotaclimat/data_processing/mediatree/i8n/extended_france/channel_program.py
+++ quotaclimat/data_processing/mediatree/i8n/extended_france/channel_program.py
@@ -1,4 +1,4 @@
-channels_programs_extended_france = {
+channels_programs_extended_france = [
     {"channel_name":"lcp","start":"07:00","end":"23:00","weekday":"*","program_grid_start":"2026-01-01","program_grid_end":"","program_name":"LCP - Public S\u00e9nat","program_type":""},
     
     {"channel_name":"tf1","start":"20:40","end":"23:59","weekday":"*","program_grid_start":"2026-01-01","program_grid_end":"","program_name":"Extended TF1","program_type":""},
@@ -15,4 +15,4 @@ channels_programs_extended_france = {
     
     {"channel_name":"france5","start":"20:00","end":"21:00","weekday":"*","program_grid_start":"2026-01-01","program_grid_end":"","program_name":"C'est Politique","program_type":""},
     {"channel_name":"france5","start":"12:30","end":"14:30","weekday":"*","program_grid_start":"2026-01-01","program_grid_end":"","program_name":"C'est à vous","program_type":""},
-}
+]

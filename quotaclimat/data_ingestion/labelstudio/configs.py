89c78854af486229dcbb42fe07622cb0380b1de0
gmguarino <gmguarino1@gmail.com>
2026-09-22 15:38:00 +0200

    extended perimeter changes prod dev

--- quotaclimat/data_ingestion/labelstudio/configs.py
+++ quotaclimat/data_ingestion/labelstudio/configs.py
@@ -1,3 +1,4 @@
+import os
 from quotaclimat.data_processing.mediatree.i8n.country import EXTENDED_FRANCE
 
 db_config = [
@@ -11,5 +12,7 @@ db_config = [
 
 # Used instead of db_config when EXTENDED_PERIMETER is true - see ingest_labelstudio.py
 db_config_extended_perimeter = [
+    {"database": "labelstudio", "countries": {24: EXTENDED_FRANCE.name}},
+] if os.getenv('DBT_ENV', 'dev') == 'prod' else [
     {"database": "labelstudio", "countries": {23: EXTENDED_FRANCE.name}},
 ]
